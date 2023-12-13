eaa3546150296ea79e6383a38e3c05d3a6e2686a
Asya <shulga-a.91@live.ru>
2023-12-13 21:44:03 +0700

    010

--- 008/Program.cs
+++ 008/Program.cs
@@ -3,3 +3,35 @@
 // завершается при вводе символа ‘q’ или при вводе
 // числа, сумма цифр которого четная.
 
+// int [] list = {1, 2, 3, 4, 5};
+
+// void reverseMassive(int[] list)
+// {
+//     int temp = 0;
+//     for (int i = 0; i < list.Length/2; i++)
+//     {
+//         temp = list[i];
+//         list[i] = list[list.Length - 1 - i];
+//         list[list.Length - 1 - i] = temp;
+//     }
+// }
+
+// Console.Write(reverseMassive(list));
+
+void reverseMassive(int[] list)
+{
+    int temp = 0;
+    for (int i = 0; i < list.Length / 2; i++)
+    {
+        temp = list[i];
+        list[i] = list[list.Length - 1 - i];
+        list[list.Length - 1 - i] = temp;
+    }
+}
+
+// Пример использования:
+int[] list = { 1, 2, 3, 4, 5 };
+reverseMassive(list);
+
+// Вывод измененного массива
+Console.WriteLine(string.Join(", ", list));
---
+++ 010/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 3: Напишите программу, которая перевернёт
+// одномерный массив (первый элемент станет
+// последним, второй – предпоследним и т.д.)
+
+int [] list = new int [10];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+FillArray(list);
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+PrintArray(list);
+Console.Write(list);
+
+void reverseMassive(int[] list)
+{
+    int temp = 0;
+    for (int i = 0; i < list.Length / 2; i++)
+    {
+        temp = list[i];
+        list[i] = list[list.Length - 1 - i];
+        list[list.Length - 1 - i] = temp;
+    }
+}
+reverseMassive(list);
+PrintArray(list);
+
